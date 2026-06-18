1d0c2ed14fb12b4630531cb1cee7050ad6c8b40d
OJ M <ojmitri5472@gmail.com>
2026-06-18 16:29:35 +0200

    Mirror messages
    I added placeholders fro the mirror messages, now eveytime you click on the mirror it will give you a random messages (there are 10 message spots in total) so yeah... the code also has comments if you need to know specific stuff !

--- Mirror.cs
+++ Mirror.cs
@@ -1,5 +1,7 @@
 using Godot;
 using System;
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
 
 public partial class Mirror : Button
 {
@@ -8,6 +10,24 @@ public partial class Mirror : Button
     Button MirrorButton;
     Button Close;
     Label MirrorLabel;
+
+    Label MirrorMessage;
+    int severity = 0;
+    string[] messages =
+    {
+
+    "Message 0",
+    "Message 1",
+    "Message 2",
+    "Message 3",
+    "Message 4",
+    "Message 5",
+    "Message 6",
+    "Message 7",
+    "Message 8",
+    "Message 9"
+
+    };
     public override void _Ready()
     {
         // Grabbing all the mirror references
@@ -16,14 +36,18 @@ public partial class Mirror : Button
         MirrorLabel = GetNode<Label>("MirrorLabel");
         Close = MirrorLabel.GetNode<Button>("Close");
 
+        MirrorMessage = GetNode<Label>("MirrorLabel");
+
         //Hiding the mirror since we only want to see it once clicked.
         HideMirrorCloseUp();
 
         //Subscribing to the relevant events
         MirrorButton.Pressed += ShowMirrorCloseUp;
         Close.Pressed += HideMirrorCloseUp;
+
+        MirrorButton.Pressed += ChangeMirrorMessage;
     }
-    
+
 
     //Showing and hiding the mirror, pretty simple.
     private void ShowMirrorCloseUp()
@@ -35,4 +59,20 @@ public partial class Mirror : Button
     {
         MirrorLabel.Hide();
     }
+
+    private void ChangeMirrorMessage()
+    {
+
+        //Random severity 0-9
+        severity = GD.RandRange(0, 9);
+
+        //Pick random message
+        int randomMessage = GD.RandRange(0, messages.Length - 1);
+
+        //Change mirror text
+        MirrorMessage.Text = messages[randomMessage];
+
+        GD.Print("Severity: " + severity);
+    }
+
 }
