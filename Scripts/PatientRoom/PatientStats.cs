0ed03b21894420fabfda91d420dd262ac07e67c9
ArwynTH <goomat15@gmail.com>
2026-08-19 18:27:35 +0200

    NAMES
    added a lottt more names, and more gender neutral ones

--- Scripts/PatientRoom/PatientStats.cs
+++ Scripts/PatientRoom/PatientStats.cs
@@ -21,20 +21,20 @@ public partial class PatientStats
 	private Room myRoom;
 
 	int dialogueIndexInner = 0;
-    int dialogueIndexOuter = 0;
+	int dialogueIndexOuter = 0;
 
 	int pulseIndex = 0;
 
 	int temperatureIndex = 0;
 
-    public TextureUnit textureUnit;
+	public TextureUnit textureUnit;
 
 	public List<ClinicAction> clinicActions = new List<ClinicAction>();
-    public string[] firstNameArray = { "John", "Jane", "Alex", "Emily", "Michael", "Sarah", "David", "Olivia", "Daniel", "Sophia", "James", "Scott", "Andrew", "Frank", "Gregory", "Ava", "Charlotte", "Sofia", "Camila", "Harper", "Taylor" };
-    public string[] lastNameArray = { "Smith", "Johnson", "Williams", "Smith", "Jones", "Miller", "Davis", "Garcia", "Miller", "Martinez", "Hernandez", "Lopez", "Gonzalez", "Wilson", "Anderson", "Thomas", "Clark", "Moore", "Jackson", "Martin", "Lee", "Harris" };
-    public string firstName;
-    public string lastName;
-    public PatientStats()
+	public string[] firstNameArray = { "Ace", "Addie", "Addison", "Adi", "Adrean", "Adrian", "Aiden", "Ainsleigh", "Ainsley", "Ainslie", "Alex", "Alexis", "Allie", "Ally", "Alta", "Amari", "Andee", "Andi", "Andie", "Andy", "Angel", "Arden", "Ari", "Ariel", "Armani", "Ash", "Ashe", "Ashley", "Ashton", "Aspen", "Aubrey", "Auburn", "Aude", "Audie", "Audy", "August", "Averi", "Avery", "Ayden", "Bailee", "Bailey", "Baylee", "Billie", "Billy", "Blair", "Bobbie", "Bobby", "Brayden", "Briggs", "Brook", "Brooklyn", "Brynn", "Cacey", "Caden", "Cadence", "Cady", "Caelin", "Caiden", "Cam", "Camden", "Camdyn", "Cameron", "Campbell", "Camron", "Camryn", "Carlen", "Carmen", "Carson", "Carter", "Casey", "Cassidy", "Cayden", "Caylin", "Cedar", "Charlie", "Chey", "Chris", "Christan", "Cobie", "Coby", "Codie", "Cody", "Coren", "Corey", "Corrie", "Cory", "Cyan", "Cyd", "Cypress", "Cyprus", "Dakota", "Dale", "Dallas", "Dana", "Dani", "Dannie", "Danny", "Darci", "Darcy", "Darrell", "Darryl", "Dayrl", "Delaney", "Demi", "Demy", "Devan", "Devin", "Devyn", "Diamond", "Dominique", "Dorian", "Drew", "Dusty", "Dylan", "Easton", "Eden", "Elisha", "Ellery", "Elliot", "Ellis", "Ellory", "Elly", "Ember", "Embry", "Embyr", "Emerson", "Emery", "Emory", "Eris", "Esme", "Esmé", "Fae", "Fen", "Fenn", "Fin", "Finley", "Finn", "Florence", "Forrest", "Fox", "Fran", "Frances", "Gabbi", "Gabby", "Genesis", "Gerrie", "Gerry", "Gris", "Griz", "Grizz", "Hailey", "Haley", "Halie", "Halley", "Harlen", "Harlow", "Harlyn", "Harper", "Hartley", "Hayden", "Haylee", "Hayley", "Hilary", "Hollis", "Hunter", "Idgie", "Iggie", "Iggy", "Indigo", "Ira", "Izzy", "Jackie", "Jade", "Jaidan", "Jaiden", "Jaidin", "Jaidyn", "Jaime", "Jaimie", "Jamie", "Jan", "Jay", "Jaydan", "Jayden", "Jaydin", "Jaydyn", "Jaylin", "Jayme", "Jean", "Jerrie", "Jerry", "Jess", "Jesse", "Jessie", "Jett", "Jo", "Jody", "Joe", "Joey", "Jonni", "Jonnie", "Jude", "Juniper", "Justice", "Kacey", "Kacie", "Kaden", "Kai", "Kaiden", "Kam", "Kameron", "Kamron", "Karter", "Kasey", "Kay", "Kayden", "Kaylin", "Kelly", "Kelsey", "Kelsie", "Kendall", "Kerri", "Kerry", "Kirby", "Kit", "Kodi", "Koree", "Koren", "Kory", "Kris", "Krishna", "Kristen", "Ky", "Kye", "Kyle", "Kyrie", "Lacy", "Lain", "Landry", "Lane", "Larkin", "Laurel", "Lauren", "Lauris", "Leaf", "Lee", "Leighton", "Lennon", "Lennox", "Lesley", "Leslie", "Lin", "Lindsay", "Linn", "Logan", "London", "Londyn", "Lonnie", "Loren", "Lorin", "Lou", "Lowe", "Luan", "Luca", "Lyn", "Lynn", "Mackenzie", "Mackinley", "Madison", "Madox", "Mallory", "Marin", "Marion", "Marley", "Marlowe", "Mars", "Mason", "Mckinley", "Meadow", "Mel", "Meredith", "Merrill", "Micah", "Micki", "Mika", "Mischa", "Misha", "Morgan", "Neely", "Nicki", "Nico", "Nikko", "Niko", "Nova", "Paige", "Paisley", "Parker", "Pat", "Pax", "Payton", "Peyton", "Phoenix", "Piper", "Presley", "Quinn", "Rae", "Rain", "Raine", "Rainn", "Raven", "Ray", "Reagan", "Reese", "Reilly", "Remi", "Rémi", "Remington", "Remy", "Rémy", "Rey", "Riley", "River", "Robbie", "Robin", "Rogue", "Rori", "Rory", "Russi", "Ryan", "Rylan", "Rylee", "Ryley", "Sage", "Salix", "Sam", "Sammie", "Sammy", "Sandi", "Sandy", "Santana", "Sasha", "Sawyer", "Scout", "Seneca", "Shannon", "Shay", "Shea", "Shelley", "Siban", "Sibán", "Sky", "Skylar", "Skyler", "Slater", "Spencer", "Stacy", "Stevie", "Storm", "Syd", "Sydney", "Tash", "Tate", "Tatum", "Tay", "Tayler", "Taylor", "Teagen", "Teal", "Teegan", "Terra", "Terry", "Tobin", "Tommie", "Toni", "Tony", "Tori", "Torrey", "Tory", "Tracey", "Traci", "Tracy", "Tristen", "Tristyn", "Tyler", "Val", "Valentine", "Viv", "Vivian", "Whitney", "Willow", "Xan", "Xander", "Yael", "Zan", "Zane", "Zephyr", "Zoe", "Zoé", "Zoë", "Zoey"  };
+	public string[] lastNameArray = { "Weber", "Moses", "Ford", "Pace", "Mullins", "Conrad", "Hamilton", "Zuniga", "Newton", "Moran", "Figueroa", "Mendez", "James", "Young", "Hensley", "Arroyo", "Conway", "Ho", "Moyer", "Ochoa", "Singleton", "Cook", "Barrett", "Mcclain", "Fitzpatrick", "Choi", "Collins", "Macias", "Parrish", "Barker", "Estrada", "Mccarty", "Valencia", "Weaver", "Clark", "Fuller", "Miles", "Andrade", "White", "Bauer", "Osborne", "Munoz", "Barron", "Watsonv", "Sullivan", "Robles", "Holloway", "Serrano", "Hale", "Knight", "Chaney", "Lawson", "Turner", "Shepard", "Jacobson", "Wilkerson", "Wiggins", "Rosales", "Bright", "Murray", "Mcclure", "Oneill", "Ritter", "Mcguire", "Brock", "Burgess", "Dickson", "Taylor", "Middleton", "Wyatt", "Bolton", "Cameron", "Maddox", "Tran", "Walsh", "Ramirez", "Hopkins", "Medina", "Higgins", "Odonnell", "Pena", "Little", "Trevino", "Buck", "Gibbs", "Rogers", "Walter", "Morton", "Meyers", "Esparza", "Stafford", "Melendez", "Dorsey", "Stone", "Bentley", "Horn", "Shea", "Mcfarland", "Lutz", "Petty", "York", "Oliver", "Glass", "Bautista", "Burke", "Robertson", "Kerr", "Harvey", "Guerra", "Hughes", "Wood", "Hancock", "Wolfe", "Mendoza", "Hines", "Weeks", "Galvan", "Manning", "Hester", "Khan", "Patrick", "Wiley", "Henderson", "Strong", "Terrell", "Madden", "Villarreal", "Peters", "Randolph", "Perry", "Ayala", "Francis", "Collier", "Bass", "Rivas", "Rodgers", "Suarez", "Saunders", "Sims", "Cain", "Frank", "Huang", "Kim", "Jenkins", "Burch", "Humphrey", "Owen", "Koch", "Waters", "Sharp", "Rowe", "Simmons", "Harding", "Mercado", "Kaufman", "Myers", "Maxwell", "Gould", "Abbott", "Burns", "Guzman", "Riley", "Ashley", "Bullock", "Hall", "Joseph", "Bowman", "Galloway", "Gibson", "Herrera", "Lin", "Santana", "Irwin", "Dyer", "Leonard", "Werner", "Villegas", "Huynh", "Clayton", "Brennan", "Lambert", "Miranda", "Vaughan", "Macdonald", "Pennington", "Aguirre", "Martin", "Hunter", "Chen", "Coffey", "Spence", "Haney", "Palmer", "Reeves", "Hurley", "Greer", "Jensen", "Hogan", "Hooper", "Calhoun", "Lara", "Clay", "Villanueva", "Walls", "Chung", "Bryant", "Mejia", "Beck", "Ewing", "Carson", "Small", "Kline", "Delgado", "Garrison", "Wilson", "Holt", "Vance", "Blackburn", "Graham", "Morrow", "Levine", "Walker", "Ferguson", "Maynard", "Morales", "Goodwin", "Mclaughlin", "Armstrong", "Franco", "Ray", "Peterson", "Oneal", "Becker", "Farley", "Carpenter", "Wolf", "Hammond", "Bailey", "Pitts", "Frost", "Patton", "Adams", "Grimes", "Reyes", "Dougherty", "Watts", "Fisher", "Oconnell", "Ayers", "Nicholson", "Baker", "Gordon", "Spears", "Daniels", "Mcmahon", "Leon", "Shaw", "Norman", "Stanton", "Marsh", "Santos", "Waller", "Rush", "Hull", "Mckenzie", "Alvarado", "Dillon", "Norris", "Edwards", "Soto", "Swanson", "Case", "Acevedo", "Wade", "House", "Mcintosh", "Montoya", "Wallace", "Kent", "Griffin", "Hatfield", "Williams", "Salas", "Brady", "Barr", "Cummings", "Finley", "Monroe", "Larson", "Church", "Fitzgerald", "Torres", "Barnett", "Huerta", "Lawrence", "Bryan", "Faulkner", "Ali", "Powers", "Mathis", "Tyler", "Clarke", "Harrington", "Estes", "Cooke", "Marks", "Stein", "Morse", "Perkins", "Gonzales", "Mcdowell", "Richardson", "Gregory", "Bartlett", "Glenn", "Ball", "Freeman", "Proctor", "Roth", "Fry", "Love", "Wells", "Andrews", "Holmes", "Cox", "Tate", "Mosley", "Cole", "Hartman", "Ross", "Pacheco", "Keith", "Alexander", "Dominguez", "Flowers", "Herring", "Avila", "Rangel", "Thomas", "Hood", "Farmer", "Lloyd", "Rubio", "Franklin", "Chandler", "Brandt", "Farrell", "Ingram", "Douglas", "Sherman", "Woodward", "Hart", "Hurst", "Cortez", "Howe", "Best", "Cruz", "Dennis", "Conner", "Navarro", "Andersen", "Gray", "Cohen", "Mcneil", "Hudson", "Montes", "Hickman", "Johns", "Pope", "Cross", "Snow", "Decker", "Holden", "Chambers", "Copeland", "Barajas", "Jennings", "Nunez", "Johnston", "Chang", "Strickland", "Cochran", "Nielsen", "Skinner", "Marquez", "Mccoy", "Underwood", "Delacruz", "King", "Mckay", "Holland", "Cunningham", "Byrd", "Roach", "Craig", "Moreno", "Montgomery", "Mccall", "Stark", "Ware" };
+	public string firstName;
+	public string lastName;
+	public PatientStats()
 	{
 		// refresh the patient's data.
 		// For just assigning random numbers, this will be overhauled later.
@@ -43,10 +43,10 @@ public partial class PatientStats
 
 		Random rnd = new Random();
 		malady = new Malady();
-        dialogueIndexInner = 0;
-        dialogueIndexOuter = 0;
-        AssignMaladyValues(MaladyList.Database.ElementAt(rnd.Next(2, 8)).Value);
-        textureUnit.unitType = malady.layerType;
+		dialogueIndexInner = 0;
+		dialogueIndexOuter = 0;
+		AssignMaladyValues(MaladyList.Database.ElementAt(rnd.Next(2, 8)).Value);
+		textureUnit.unitType = malady.layerType;
 		if (malady.layerType == TextureType.Normal || malady.layerType == TextureType.Top)
 		{
 			textureUnit.SaveMaladySet(malady.visualKey, malady.layerType);
@@ -58,12 +58,12 @@ public partial class PatientStats
 		isAlive = true;
 		patientID = rnd.Next(1, 1000).ToString("D3");//  "D3" writes the ID as a 3-digit string  005 
 		age = rnd.Next(18, 91); // random ages of patients between 18 and 90 seemed appropriate for the game
-        firstName = firstNameArray[rnd.Next(0, firstNameArray.Length)]; // assigns random firstname from the array. 0 to the length of all the names in the array
-        lastName = lastNameArray[rnd.Next(0, lastNameArray.Length)]; // assigns random lastname from the array. 0 to the length of all the names in the array.
+		firstName = firstNameArray[rnd.Next(0, firstNameArray.Length)]; // assigns random firstname from the array. 0 to the length of all the names in the array
+		lastName = lastNameArray[rnd.Next(0, lastNameArray.Length)]; // assigns random lastname from the array. 0 to the length of all the names in the array.
 
 
-        // Assigning a random color to the patient's portrait, This will be changed later when we have actual portraits.
-        PortraitColor = new Color(
+		// Assigning a random color to the patient's portrait, This will be changed later when we have actual portraits.
+		PortraitColor = new Color(
 			1,
 			1,
 			1
@@ -108,7 +108,7 @@ public partial class PatientStats
 		}*/
 		if (malady.dialogueSymptoms.Count > 0)
 		{
-            string returnDialogue = malady.dialogueSymptoms[dialogueIndexInner].quotes[dialogueIndexOuter];
+			string returnDialogue = malady.dialogueSymptoms[dialogueIndexInner].quotes[dialogueIndexOuter];
 
 			//if(malady.dialogueSymptoms[dialogueIndexInner].quotes.Count > 0)
 			if(malady.dialogueSymptoms.Count > 1)
@@ -116,33 +116,33 @@ public partial class PatientStats
 				if(dialogueIndexInner + 1 >= malady.dialogueSymptoms.Count)
 				{
 					dialogueIndexInner = 0;
-                    if (dialogueIndexOuter + 1 >= malady.dialogueSymptoms[dialogueIndexInner].quotes.Count)
-                    {
-                        dialogueIndexOuter = 0;
-                    }
-                    else
-                    {
-                        dialogueIndexOuter++;
-                    }
-                }
+					if (dialogueIndexOuter + 1 >= malady.dialogueSymptoms[dialogueIndexInner].quotes.Count)
+					{
+						dialogueIndexOuter = 0;
+					}
+					else
+					{
+						dialogueIndexOuter++;
+					}
+				}
 				else
 				{
-                    dialogueIndexInner++;
-                   /* if (dialogueIndexOuter + 1 >= malady.dialogueSymptoms[dialogueIndexInner].quotes.Count)
+					dialogueIndexInner++;
+				   /* if (dialogueIndexOuter + 1 >= malady.dialogueSymptoms[dialogueIndexInner].quotes.Count)
 					{
 						GD.Print($"Outer plus one is {dialogueIndexOuter + 1}, count is {malady.dialogueSymptoms[dialogueIndexInner].quotes.Count}");
 						dialogueIndexOuter = 0;
-                    }
-                    else
-                    {
-                        dialogueIndexOuter++;
-                    }*/
-                }
-            }
+					}
+					else
+					{
+						dialogueIndexOuter++;
+					}*/
+				}
+			}
 
-            return returnDialogue;
-        }
-        return "...";
+			return returnDialogue;
+		}
+		return "...";
 	}
 
 	public virtual string GetAdmittedDialogue()
@@ -182,15 +182,15 @@ public partial class PatientStats
 		if (malady.temperatureSymptoms.Count > 0)
 		{
 			string returnDialogue = malady.temperatureSymptoms[0].quotes[temperatureIndex];
-            if (temperatureIndex + 1 >= malady.temperatureSymptoms[0].quotes.Count)
-            {
-                temperatureIndex = 0;
-            }
-            else
-            {
-                temperatureIndex++;
-            }
-            return returnDialogue;
+			if (temperatureIndex + 1 >= malady.temperatureSymptoms[0].quotes.Count)
+			{
+				temperatureIndex = 0;
+			}
+			else
+			{
+				temperatureIndex++;
+			}
+			return returnDialogue;
 		}
 		return "Not too hot, not too cold!";
 	}
